6a937325c90853873ae88f5fe52826dd88c4b590
monten1234 <touch26.takehiro@icloud.com>
2023-08-30 17:15:50 +0900

    fuck

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ import requests
 
 SUPABASE_URL = "https://clwmzkijmvdpxsgjwniz.supabase.co"
 SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsd216a2lqbXZkcHhzZ2p3bml6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMzMDU4MzIsImV4cCI6MjAwODg4MTgzMn0.2kpa137KbwfJTUQ2xQLb13SxYaHkCKgVkWaLc3soRDg"
-BUCKET_NAME = "test1"
+BUCKET_NAME = "sumple2"
 
 def upload_to_supabase(file_contents, file_name):
     headers = {
