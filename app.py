74613486f6a4bd70c84bddb05ec7ad541097c611
monten1234 <touch26.takehiro@icloud.com>
2023-08-31 11:51:20 +0900

    fuck

--- app.py
+++ app.py
@@ -56,9 +56,9 @@ if file:
 
     # ファイルのアップロード
     bucket_name = "sumple2"
-    object_name = f"/sumple2/{file.name}"
+    object_name = f"/aaa/{file.name}"
     new_file = file.read()  # アップロードするファイルのバイナリデータ
-    upload_result = supabase.storage.from_(bucket_name).upload(bucket_name, new_file)
+    upload_result = supabase.storage.from_(bucket_name).upload(object_name, new_file)
 
     if upload_result:
         st.success("File uploaded successfully!")
