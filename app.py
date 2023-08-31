1224c41d85c7c11d00420429bce71a537916d327
monten1234 <touch26.takehiro@icloud.com>
2023-08-31 12:05:47 +0900

    fuck

--- app.py
+++ app.py
@@ -36,9 +36,8 @@ import streamlit as st
 import os
 from supabase import create_client, Client
 
-# Supabase URLとAPIキーを環境変数から取得
-url = "https://clwmzkijmvdpxsgjwniz.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsd216a2lqbXZkcHhzZ2p3bml6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMzMDU4MzIsImV4cCI6MjAwODg4MTgzMn0.2kpa137KbwfJTUQ2xQLb13SxYaHkCKgVkWaLc3soRDg"
+url: str = "https://clwmzkijmvdpxsgjwniz.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsd216a2lqbXZkcHhzZ2p3bml6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMzMDU4MzIsImV4cCI6MjAwODg4MTgzMn0.2kpa137KbwfJTUQ2xQLb13SxYaHkCKgVkWaLc3soRDg"
 # Supabaseクライアントを作成
 supabase: Client = create_client(url, key)
 
