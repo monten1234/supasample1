e1a7497aa6acff1270d0cc8f37b47a796b4b12be
monten1234 <touch26.takehiro@icloud.com>
2023-09-01 14:00:15 +0900

    fuck

--- app.py
+++ app.py
@@ -58,7 +58,38 @@ if file:
 
     if upload_result:
         get_url = supabase.storage.from_(bucket_name).get_public_url(object_name)
-        print(get_url)
+        st.write(get_url)
         st.success("File uploaded successfully!")
     else:
         st.error("File upload failed.")
+
+# import streamlit as st
+# import os
+# from supabase import create_client, Client
+
+# url: str = "https://clwmzkijmvdpxsgjwniz.supabase.co"
+# key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsd216a2lqbXZkcHhzZ2p3bml6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMzMDU4MzIsImV4cCI6MjAwODg4MTgzMn0.2kpa137KbwfJTUQ2xQLb13SxYaHkCKgVkWaLc3soRDg"
+# # Supabaseクライアントを作成
+# supabase: Client = create_client(url, key)
+
+# # Streamlitアプリの設定
+# st.title("Supabase Storage Uploader")
+# st.write("Upload a file to Supabase Storage")
+# # ファイルアップロードのインターフェース
+# file = st.file_uploader("Choose a file", type=["png", "jpg", "jpeg", "xlsx"])
+# if file:
+#     st.write("File selected:", file.name)
+
+#     # ファイルのアップロード
+#     bucket_name = "sumple2"
+#     object_name = f"aaa/{file.name}"
+#     # urllib.parse.urljoin('aaa','')
+#     new_file = file.read()  # アップロードするファイルのバイナリデータ
+#     upload_result = supabase.storage.from_(bucket_name).upload(object_name, new_file)
+
+#     if upload_result:
+#         get_url = supabase.storage.from_(bucket_name).get_public_url(object_name)
+#         print(get_url)
+#         st.success("File uploaded successfully!")
+#     else:
+#         st.error("File upload failed.")
